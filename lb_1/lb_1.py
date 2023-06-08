f20dec2abb1c85ad4034c8ee9ac705c8314aaeff
Arkadii <arkadiimomot@gmail.com>
2023-06-08 13:55:19 +0300

    refactoring

--- lb_1/lb_1.py
+++ lb_1/lb_1.py
@@ -93,85 +93,86 @@ def calc_standard_deviation(distribution_dict: dict) -> float:
     variance=calc_variance(distribution_dict)
     return math.sqrt(variance)
 
+if __name__ == "__main__":
+    # * Полігон
+    # plt.plot(od.keys(), od.values())
+    # plt.suptitle("Полігон")
 
-# * Полігон
-# plt.plot(od.keys(), od.values())
-# plt.suptitle("Полігон")
+    # * Гістограма
+    # fig = plt.figure()
+    # ax = fig.add_subplot (111)
+    # ax.hist (sample, edgecolor='black')
+    # plt.suptitle("Гістограма")
 
-# * Гістограма
-# fig = plt.figure()
-# ax = fig.add_subplot (111)
-# ax.hist (sample, edgecolor='black')
-# plt.suptitle("Гістограма")
+    # * Розмаху
+    # plt.boxplot(od.keys(), od.values())
+    # plt.suptitle("Розмаху")
 
-# * Розмаху
-# plt.boxplot(od.keys(), od.values())
-# plt.suptitle("Розмаху")
+    # * Парета
+    # df = pd.DataFrame.from_dict({'value': odv.values()})
 
-# * Парета
-# df = pd.DataFrame.from_dict({'value': odv.values()})
-
-# df = df.sort_values(by='value',ascending=False)
-# df["cumpercentage"] = df["value"].cumsum()/df["value"].sum()*100
-
-
-# fig, ax = plt.subplots()
-# ax.bar(df.index, df["value"], color="C0")
-# ax2 = ax.twinx()
-# ax2.plot(df.index, df["cumpercentage"], color="C1", marker="D", ms=7)
-# ax2.yaxis.set_major_formatter(PercentFormatter())
-
-# ax.tick_params(axis="y", colors="C0")
-# ax2.tick_params(axis="y", colors="C1")
-# plt.suptitle("Парета")
+    # df = df.sort_values(by='value',ascending=False)
+    # df["cumpercentage"] = df["value"].cumsum()/df["value"].sum()*100
 
 
-# * Кругова
-# * Створення списку для подальшої роботи
-data = []
-for key, value in od.items():
-    if key >= 0:
-        data.append((key, value))
-    else:
-        data.append((-key, value))
+    # fig, ax = plt.subplots()
+    # ax.bar(df.index, df["value"], color="C0")
+    # ax2 = ax.twinx()
+    # ax2.plot(df.index, df["cumpercentage"], color="C1", marker="D", ms=7)
+    # ax2.yaxis.set_major_formatter(PercentFormatter())
 
-# * Створення кругової діаграми з абсолютними значеннями
-plt.figure(figsize=(7, 6))
-plt.pie([x[0] for x in data], labels=[str(x[0]) for x in data])
+    # ax.tick_params(axis="y", colors="C0")
+    # ax2.tick_params(axis="y", colors="C1")
+    # plt.suptitle("Парета")
 
 
-# * Функція для зворотнього відображення значень на графіку
-def backmapping(value):
-    for x in data:
-        if x[0] == value:
-            if key < 0:
-                return -x[0]
-            else:
-                return x[0]
+    # * Кругова
+    # * Створення списку для подальшої роботи
+    data = []
+    for key, value in od.items():
+        if key >= 0:
+            data.append((key, value))
+        else:
+            data.append((-key, value))
+
+    # * Створення кругової діаграми з абсолютними значеннями
+    plt.figure(figsize=(7, 6))
+    plt.pie([x[0] for x in data], labels=[str(x[0]) for x in data])
 
 
-# * Додавання зворотнього відображення на графіку
-plt.suptitle("Кругова")
-plt.gca().set_aspect('equal')
-# plt.gca().legend(
-#     [f"{backmapping(v)} ({v:.2f})" for v in [x[0] for x in data]],
-#     title="Values",
-#     loc='upper right'
-# )
-
-plt.show()
+    # * Функція для зворотнього відображення значень на графіку
+    def backmapping(value):
+        for x in data:
+            if x[0] == value:
+                if key < 0:
+                    return -x[0]
+                else:
+                    return x[0]
 
 
+    # * Додавання зворотнього відображення на графіку
+    plt.suptitle("Кругова")
+    plt.gca().set_aspect('equal')
+    # plt.gca().legend(
+    #     [f"{backmapping(v)} ({v:.2f})" for v in [x[0] for x in data]],
+    #     title="Values",
+    #     loc='upper right'
+    # )
+
+    plt.show()
+
+    #* Вивід результатів
+    print(f"Номер варіанту: {variant}\nВідповідно до варіанту n = {n} sigma = {sigma}")
+    print(f"Математичне сподівання: {calculate_mean(sample_dict).round(4)}")
+    print(f"Вибіркове середнє:{calculate_mean(sample_dict)}")
+    print(f"Медіана:{calc_median(sample_dict)}")
+    print(f"Мода:{calc_mode(sample_dict)}")
+    print(f"Дисперсія:{calc_variance(sample_dict)}")
+    print(f"Середньоквадратичне відхилення:{calc_standard_deviation(sample_dict)}")
+    print_dict(sample_dict)
 
 
 
 
-#* Вивід результатів
-print(f"Номер варіанту: {variant}\nВідповідно до варіанту n = {n} sigma = {sigma}")
-print(f"Математичне сподівання: {calculate_mean(sample_dict).round(4)}")
-print(f"Вибіркове середнє:{calculate_mean(sample_dict)}")
-print(f"Медіана:{calc_median(sample_dict)}")
-print(f"Мода:{calc_mode(sample_dict)}")
-print(f"Дисперсія:{calc_variance(sample_dict)}")
-print(f"Середньоквадратичне відхилення:{calc_standard_deviation(sample_dict)}")
-print_dict(sample_dict)
+
+
